812ba18708487c49444131c22c637bb74ec7541c
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-02 20:12:27 +0530

    ch

--- start-webapi.ps1
+++ start-webapi.ps1
@@ -1,5 +1,5 @@
 # Change directory to the API application path
-Set-Location -Path "C:\WebApi"
+Set-Location -Path "C:\HomeApplication"
 
 # Start the API using the appropriate command
-Start-Process -FilePath "dotnet" -ArgumentList "PipelineTesting.dll" -NoNewWindow -PassThru
+Start-Process -FilePath "dotnet" -ArgumentList "HomeApi.dll" -NoNewWindow -PassThru
